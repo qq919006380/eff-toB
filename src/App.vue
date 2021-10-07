71159dd6fa834b5f19aac3d82ce4e2a2d2e8247f
weibo <919006380@qq.com>
2021-10-07 19:37:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app-container">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App'
+})
+</script>
